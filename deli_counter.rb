8e29381ffea08145f6a4afaf7f9f537b42450768
adeeba reaz <adeebarivera@outlook.com>
2020-04-22 23:39:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name) 
-  puts "Welcome, Ada. You are number 1 in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 
