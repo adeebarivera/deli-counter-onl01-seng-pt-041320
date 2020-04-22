a8e65f67817629ecd289156ede4494b1e11d5203
adeeba reaz <adeebarivera@outlook.com>
2020-04-22 23:23:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,25 @@
-katz_deli = []
 #input integer representing number of people
 #if number of people is 0, puts out a string "the line is currently empty."
 def line(katz_deli)
+  local = []
   line_call = "The line is currently: "
   if katz_deli.empty? 
     puts "The line is currently empty."
   else  
-      katz_deli.each_with_index do |index, name| 
-        katz_deli.push("#{index}. #{name}")
+      katz_deli.each_with_index do |name, index| 
+        local.push("#{index + 1}. #{name}")
 end
-  puts line_call + katz_deli.join(" ")
+  puts line_call + local.join(" ")
 end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli.count(name) 
+  puts "Welcome, Ada. You are number 1 in line."
+end
+
+
+
   
   
   
